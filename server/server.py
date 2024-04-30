005bcfcc54e51d8fdfc61c206d30713174dc346b
swannekim <swannekim@snu.ac.kr>
2024-04-30 08:04:37 +0000

    basic framework setup commit

---
+++ server/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+# app instance
+app = Flask(__name__)
+CORS(app) # allows port 8080 in use
+
+# app routing @ /api/home
+# "Hello World!" will appear on http://127.0.0.1:8080/api/home
+@app.route("/api/home", methods={'GET'})
+def return_home():
+    return jsonify({
+        'testmessage': "Hello World!",
+        'titleofweb': "KRISO 2024 Visualization",
+        'shiptype': ['cargo', 'passenger', 'tanker', 'government']
+    })
+
+# app running
+if __name__ == "__main__":
+    app.run(debug=True, port=8080) # dev mode
+    # app.run() # deploy production mode
